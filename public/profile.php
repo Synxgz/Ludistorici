ff99536a5924790a43df0ff7b61b9e3b473b8632
Akt0o <valentin.colindre@gmail.com>
2023-10-27 10:55:44 +0200

    (ajout profile.php) (ajout stylep.css)

---
+++ public/profile.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="assets/css/stylep.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+<body>
+    <img id="p_banner" src="">
+    <div id="p_main_body">
+        <div id="p_profile_info">
+            <img id="p_profile" src="">
+            <h2>Nom</h2>
+            <div>
+                <p>Abonnés : </p>
+                <p>Articles : </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
